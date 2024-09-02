ff8f5601e40639ba09443d9171394f5ed7937b05
Jeanhela Nazate <jcnazate@espe.edu.ec>
2024-09-02 00:07:27 -0500

    commit

--- localGPT/model_instance.py
+++
@@ -1,8 +0,0 @@
-from models import ModelManager
-from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain_chroma import Chroma
-from constants import EMBEDDING_MODEL_NAME, PERSIST_DIRECTORY, CHROMA_SETTINGS
-
-embeddings = HuggingFaceEmbeddings(model_name=EMBEDDING_MODEL_NAME)
-db = Chroma(persist_directory=PERSIST_DIRECTORY, embedding_function=embeddings, client_settings=CHROMA_SETTINGS)
-model_manager = ModelManager(db)
